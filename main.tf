c62a96e72b69259b1165905c0d6d0695772ddc2e
Akhileshdevops-insider <akhiles.ku.sri@gmail.com>
2025-07-30 18:17:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,42 +18,29 @@ backend "azurerm" {
 
 
 provider "azurerm" {
-
   features {}
-
   subscription_id = "25a58ceb-2570-42e2-9d35-00b46979c51a"
 }
 
-
-
-
-resource "azurerm_resource_group" "example1" {
+resource "azurerm_resource_group" "rg2" {
   name     = "GRpipe"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "exstg" {
-
   name                     = "strgpipe"
-  resource_group_name      = azurerm_resource_group.example1.name
-  location                 = azurerm_resource_group.example1.location
+  resource_group_name      = azurerm_resource_group.rg2.name
+  location                 = azurerm_resource_group.rg2.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
-
-
 }
 
-
-
 resource "azurerm_storage_account" "exstg1" {
-
   name                     = "strgpipe1"
-  resource_group_name      = azurerm_resource_group.example1.name
-  location                 = azurerm_resource_group.example1.location
+  resource_group_name      = azurerm_resource_group.rg2.name
+  location                 = azurerm_resource_group.rg2.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
-
-
 }
 
 
